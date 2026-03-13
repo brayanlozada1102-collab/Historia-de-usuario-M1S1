8e04d3ea43ecbef10a1d45197ebcce5ba72d6f81
Brayan Lozada <brayanlozada1102@gmail.com>
2026-03-13 01:59:07 -0500

    actualizacion 1.0/ Dia 13 de marzo

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
+from logicinventory import sales_register , show_summary
+# INVENTORY REGISTRATION PROGRAM
+daily_revenue = 0
+daily_sales = []
+start = True
+print("Welcome to the inventory registration system \n -----------------------")
+start = True
 
+# Request and validate the price (must be a decimal/float)
+while start:
+    # Request the product name
+    name = input("Enter the product name: ")
+    try:
+        price = float(input("Enter the unit price: "))
+        if price < 0:
+            print("Error: The price cannot be negative.")
+            continue
+    except ValueError:
+        print("Error: Please enter a numeric value for the price.")
+ # Request and validate the quantity (must be an integer/int)
+    try:
+        quantity = int(input("Enter the inventory quantity: "))
+        if quantity < 0:
+            print("Error: The quantity cannot be negative.")
+            continue
+    except ValueError:
+        print("Error: Please enter a whole number for the quantity.")
+
+    sales_register(name,quantity,price,daily_sales)
+  # Mathematical operation: Calculate total cost
+  # Price (float) is multiplied by quantity (int)
+    daily_revenue = daily_revenue + (quantity*price)
+
+    try:
+       switch = input("Do you want to continue recording sales? Yes/No: ").lower()
+       if switch != "yes":
+           start = False
+    except ValueError:
+        print("Error try again")
+
+
+
+# Display results in console with the requested format
+print("\nInventory \n-----------------------")
+show_summary(daily_sales)
+print(f"The total inventory values: ${daily_revenue}")
+
+# General comment:
+# This program allows registering a product by capturing its name, price, and quantity.
+# It includes data type validation to ensure the total cost calculation 
+# (price * quantity) is performed without execution errors.
